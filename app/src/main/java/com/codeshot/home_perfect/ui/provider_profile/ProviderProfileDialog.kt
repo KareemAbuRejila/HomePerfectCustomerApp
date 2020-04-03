6873ed6fae7bd98880f32d0a3c7375546eb50675
KareemAbuRejila <KareemAbuRejila@gmail.com>
2020-04-03 20:55:47 +0300

    Initial commit Gradient Images

--- app/src/main/java/com/codeshot/home_perfect/ui/provider_profile/ProviderProfileDialog.kt
+++ app/src/main/java/com/codeshot/home_perfect/ui/provider_profile/ProviderProfileDialog.kt
@@ -33,7 +33,8 @@ class ProviderProfileDialog : SuperBottomSheetFragment {
     private val TAG = "ProviderProfileDialog"
 
     private lateinit var fcmService: IFCMService
-    private var provider: Provider?=null
+    private var provider: Provider? = null
+
     constructor() : super()
     constructor(provider: Provider?) : super() {
         this.provider = provider
@@ -59,46 +60,50 @@ class ProviderProfileDialog : SuperBottomSheetFragment {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-                dialogProviderProfileBinding.provider = provider
-                dialogProviderProfileBinding.contentProfile.provider=provider
-                checkProviderStatus()
+        dialogProviderProfileBinding.provider = provider
+        dialogProviderProfileBinding.contentProfile.provider = provider
+        checkProviderStatus()
 
 
 //        dialogProviderProfileBinding.fullDialogClose.setOnClickListener { super.dismiss() }
         dialogProviderProfileBinding.btnBookingDialog.setOnClickListener {
 //            bookProvider(provider!!.id!!)
-            val bookingDialog=BookingProviderDialog(provider)
-            bookingDialog.show(childFragmentManager,"BookingDialog")
-            }
-
-    }
-
-    private fun checkProviderStatus() {
-            PROVIDERS_REF.document(provider!!.id!!).addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
-            if (firebaseFirestoreException!=null)
-                return@addSnapshotListener
-            else{
-                val provider=documentSnapshot!!.toObject(Provider::class.java)
-                if (provider!!.online){
-                    dialogProviderProfileBinding.btnBookingDialog.text=requireContext().resources.getString(R.string.book)
-                    dialogProviderProfileBinding.btnBookingDialog.visibility=View.VISIBLE
-//                    dialogProviderProfileBinding.btnBookingDialog.setTextColor(requireActivity().resources.getColor(android.R.color.holo_green_light))
-                    dialogProviderProfileBinding.btnBookingDialog.isEnabled=true
-                    dialogProviderProfileBinding.contentProfile.status.background=requireContext().resources.getDrawable(R.drawable.ic_status_on)
-                }else{
-                    dialogProviderProfileBinding.btnBookingDialog.visibility=View.GONE
-                    dialogProviderProfileBinding.contentProfile.status.background=requireContext().resources.getDrawable(R.drawable.ic_status_off)
-
-                }
-
-
-            }
+            val bookingDialog = BookingProviderDialog(provider)
+            bookingDialog.show(childFragmentManager, "BookingDialog")
         }
 
     }
 
+    private fun checkProviderStatus() {
+        PROVIDERS_REF.document(provider!!.id!!)
+            .addSnapshotListener { documentSnapshot, firebaseFirestoreException ->
+                if (firebaseFirestoreException != null)
+                    return@addSnapshotListener
+                else {
+                    val provider = documentSnapshot!!.toObject(Provider::class.java)
+                    if (provider!!.online) {
+                        dialogProviderProfileBinding.btnBookingDialog.text =
+                            requireContext().resources.getString(R.string.book)
+                        dialogProviderProfileBinding.btnBookingDialog.visibility = View.VISIBLE
+//                    dialogProviderProfileBinding.btnBookingDialog.setTextColor(requireActivity().resources.getColor(android.R.color.holo_green_light))
+                        dialogProviderProfileBinding.btnBookingDialog.isEnabled = true
+                        dialogProviderProfileBinding.contentProfile.status.background =
+                            requireContext().resources.getDrawable(R.drawable.ic_status_on)
+                    } else {
+                        dialogProviderProfileBinding.btnBookingDialog.visibility = View.GONE
+                        dialogProviderProfileBinding.contentProfile.status.background =
+                            requireContext().resources.getDrawable(R.drawable.ic_status_off)
+
+                    }
+
+
+                }
+            }
+
+    }
+
     override fun getPeekHeight(): Int {
-        return super.getPeekHeight()+100
+        return super.getPeekHeight() + 100
     }
 
 
@@ -115,48 +120,58 @@ class ProviderProfileDialog : SuperBottomSheetFragment {
                             val providerToken = document.toObject(Token::class.java)
                             val userToken = CURRENT_TOKEN
                             val msgContent: MutableMap<String, String> = HashMap()
-                            msgContent["title"]="Booking"
+                            msgContent["title"] = "Booking"
                             msgContent["userToken"] = userToken
-                            msgContent["userId"]= CURRENT_USER_PHONE
-                            msgContent["userKey"]= CURRENT_USER_KEY
-                            msgContent["userName"]= CURRENT_USER_NAME
-                            val request=Request(CURRENT_USER_PHONE,providerID,"waiting")
+                            msgContent["userId"] = CURRENT_USER_PHONE
+                            msgContent["userKey"] = CURRENT_USER_KEY
+                            msgContent["userName"] = CURRENT_USER_NAME
+                            val request = Request(CURRENT_USER_PHONE, providerID, "waiting")
                             Common.REQUESTS_REF.add(request)
                                 .addOnSuccessListener {
-                                msgContent["requestId"]=it.id
-                                val dataMessage = DataMessage(providerToken.token, msgContent)
-                                val providerTask=PROVIDERS_REF.document(providerID)
-                                    .update("requests",FieldValue.arrayUnion(it.id))
-                                val userTask= USERS_REF.document(CURRENT_USER_PHONE)
-                                    .update("requests",FieldValue.arrayUnion(it.id))
-                                Tasks.whenAllSuccess<Toast>(providerTask,userTask)
-                                    .addOnSuccessListener {
-                                        fcmService.sendMessage(dataMessage)
-                                            .enqueue(object : Callback<FCMResponse> {
-                                                override fun onResponse(call: Call<FCMResponse>,response: Response<FCMResponse>) {
-                                                    if (response.isSuccessful) {
-                                                        Toast.makeText(context, "Success", Toast.LENGTH_LONG)
-                                                            .show()
-                                                    } else {
+                                    msgContent["requestId"] = it.id
+                                    val dataMessage = DataMessage(providerToken.token, msgContent)
+                                    val providerTask = PROVIDERS_REF.document(providerID)
+                                        .update("requests", FieldValue.arrayUnion(it.id))
+                                    val userTask = USERS_REF.document(CURRENT_USER_PHONE)
+                                        .update("requests", FieldValue.arrayUnion(it.id))
+                                    Tasks.whenAllSuccess<Toast>(providerTask, userTask)
+                                        .addOnSuccessListener {
+                                            fcmService.sendMessage(dataMessage)
+                                                .enqueue(object : Callback<FCMResponse> {
+                                                    override fun onResponse(
+                                                        call: Call<FCMResponse>,
+                                                        response: Response<FCMResponse>
+                                                    ) {
+                                                        if (response.isSuccessful) {
+                                                            Toast.makeText(
+                                                                context,
+                                                                "Success",
+                                                                Toast.LENGTH_LONG
+                                                            )
+                                                                .show()
+                                                        } else {
+                                                            Toast.makeText(
+                                                                context,
+                                                                "Request Failed sent!",
+                                                                Toast.LENGTH_SHORT
+                                                            ).show()
+                                                        }
+                                                    }
+
+                                                    override fun onFailure(
+                                                        call: Call<FCMResponse>,
+                                                        t: Throwable
+                                                    ) {
+                                                        Log.e("ERROR REQUEST SENT ", t.message!!)
                                                         Toast.makeText(
                                                             context,
-                                                            "Request Failed sent!",
+                                                            "ERROR REQUEST SENT! " + t.message,
                                                             Toast.LENGTH_SHORT
                                                         ).show()
                                                     }
-                                                }
-
-                                                override fun onFailure(call: Call<FCMResponse>,t: Throwable) {
-                                                    Log.e("ERROR REQUEST SENT ", t.message!!)
-                                                    Toast.makeText(
-                                                        context,
-                                                        "ERROR REQUEST SENT! " + t.message,
-                                                        Toast.LENGTH_SHORT
-                                                    ).show()
-                                                }
-                                            })
-                                    }
-                            }
+                                                })
+                                        }
+                                }
                         }
                     }
                 }
